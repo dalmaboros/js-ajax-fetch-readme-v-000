ff3d7f6d6e519cc7de2fa8b6b2ed55593912ddc6
Dalma Boros <dalma.boros@gmail.com>
2018-03-21 09:41:25 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 const app = "I don't do much.";
+
+const token = "5a2298a50d60710d05009ad4aefd9481671c5bf7";
+
+fetch('https://api.github.com/user/repos', {
+  headers: {
+    Authorization: `token ${token}`
+  }
+}).then(res => res.json()).then(json => console.log(json));
